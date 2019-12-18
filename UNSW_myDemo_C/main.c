ae529116d2311641cf1d4de7cab2d0ada50d1a0b
Zhaokun1997 <357610140@qq.com>
2019-12-18 21:44:31 +1100

    Initial Commit

---
+++ UNSW_myDemo_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  UNSW_myDemo_C
+//
+//  Created by Steve on 18/12/19.
+//  Copyright © 2019 Steve. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
